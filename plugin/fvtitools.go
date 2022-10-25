5f87d531b20ddd6cf2376304fa237fcbe3b2cf4f
Titus <62823952+5i03@users.noreply.github.com>
2022-10-25 23:58:09 +0800

    注册插件

--- plugin/fvtitools.go
+++ plugin/fvtitools.go
@@ -3,13 +3,12 @@ package fvtitools
 import (
 		"math/rand"
 		"time"
-	
 		ctrl "github.com/FloatTech/zbpctrl"
 		"github.com/FloatTech/zbputils/control"
 		"github.com/FloatTech/zbputils/ctxext"
 		zero "github.com/wdvxdr1123/ZeroBot"
 		"github.com/wdvxdr1123/ZeroBot/message"
-		)
+)
 
 // FVTI is a function that returns a function that can be used to send a message to the group
 var examplelimit = ctxext.NewLimiterManager(time.Second*10, 1)
@@ -27,10 +26,6 @@ func init(){
 		},
 
 
-	}
-	// 注册一个命令	)
-	engine.OnFullMatchGroup([]string{"fvtitools"}, zero.AdminPermission).SetBlock(true).SetPriority(20).Handle(func(ctx *zero.Ctx) {
-		ctx.SendChain(message.Text("fvtitools"))
 	}
 }
 	
